245c4f803f32d48c629e3921347ccd2f4413f02e
uzoolove <uzoolove@gmail.com>
2020-10-13 10:18:26 +0900

    .

--- ch03/ex03-14.js
+++ ch03/ex03-14.js
@@ -19,5 +19,11 @@ for(var i in arr){
 }
 
 // 배열의 모든 요소 출력(for-of, ECMA6)
+for(var elem of arr){
+  console.log(elem);
+}
 
 // 배열의 모든 요소 출력(forEach(), ECMA5)
+arr.forEach(function(elem, i){
+  console.log(i, elem);
+});
--- etc/functiontest.js
+++ etc/functiontest.js
@@ -1,8 +1,35 @@
 // 10 + 100의 결과를 출력한다.
-
+// TODO: hoisting 단원. 함수 선언버다 호출을 먼저 할 수 있다.
+add();
+function add(){
+  var n1 = 10;
+  var n2 = 100;
+  var result = n1 + n2;
+  console.log(n1 + ' + ' + n2 + ' = ' + result);
+}
+add();
 // 전달받은 숫자와 100의 합계를 출력한다.
-
+function add100(n1){
+  var n2 = 100;
+  var result = n1 + n2;
+  // ECMA6 Template literal
+  console.log(`${n1} + ${n2} = ${result}`);
+}
+add100(20);
+add100(30);
 // 전달받은 두 수의 합계를 출력한다.
-
+function sum(n1, n2){
+  var result = n1 + n2;
+  console.log(`${n1} + ${n2} = ${result}`);
+}
+sum(40, 50);
+var returnSum = sum(60, 70);
+console.log(returnSum);
 // 전달받은 두 수의 합계를 반환한다.
-
+function getSum(n1, n2){
+  var result = n1 + n2;
+  return (`${n1} + ${n2} = ${result}`);
+}
+var result = getSum(80, 90);
+console.log(result);
+console.log(getSum(10, 20));
