d08d9e0364a1e24127e73a5ffd5df20cc1fecf5f
uzoolove <uzoolove@gmail.com>
2020-10-13 14:03:54 +0900

    .

--- ch03/ex03-20.js
+++ ch03/ex03-20.js
@@ -6,5 +6,31 @@ function printArr(arr){
 	}
 }
 
-var arr = ['zero', 'one', 'two'];
+// 지정한 객체의 모든 속성을 출력한다.
+function printObj(obj){
+  for(var prop in obj){
+    if(obj.hasOwnProperty(prop)){
+      console.log(prop, obj[prop]);
+    }    
+  }
+}
 
+var arr = ['zero', 'one', 'two'];
+arr.push('three');
+arr.push('four');
+arr.pop();
+arr.shift();
+
+// 배열에 속성 추가
+arr.name = 'kim';
+// 대입연산자로 속성을 추가하면 enumerable, writable, configurable이 true로 지정
+// arr['age'] = 30;
+
+Object.defineProperty(arr, 'age', {
+  value: 30,
+  enumerable: true, // 열거 가능한지 여부(Object.keys(arr), Object.values(arr), Object.entries(), for-in), 생략 시 false
+  writable: true, // 대입 연산자로 value 수정 가능하지 여부(상수형 변수), 생략시 false
+  configurable: true, // Object.defineProperty()로 속성값 설정이 가능한지 여부, 생략시 false
+});
+
+printObj(arr);
--- etc/localvariablescope.js
+++ etc/localvariablescope.js
@@ -1,8 +1,14 @@
 // 지정한 두 값의 최대값을 반환한다.
 function max(n1, n2){
-	var maxNum;
+  // TODO: hoisting 단원. var로 선언한 지역변수가 함수단위의 스코를 갖는 이유
+	// var maxNum;
 	
-	
+  if(n1 > n2){
+    var maxNum = n1;
+  }else{
+    var maxNum = n2;
+  }
+  
 	return maxNum;
 }
 
