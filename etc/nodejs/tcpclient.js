690ce357d450c66810df37eb18cca6ba39c79f81
uzoolove <uzoolove@gmail.com>
2020-10-14 15:21:39 +0900

    .

--- etc/nodejs/tcpclient.js
+++ etc/nodejs/tcpclient.js
@@ -1,4 +1,7 @@
 const net = require('net');
 var socket = net.createConnection(8000, 'localhost');
-socket.write('hello');
-// process.stdin.pipe(socket);
+// socket.write('hello');
+process.stdin.pipe(socket);
+socket.on('data', function(data){
+  console.log(data.toString());
+});
--- etc/nodejs/tcpserver.js
+++ etc/nodejs/tcpserver.js
@@ -3,6 +3,11 @@ net.createServer(function(socket){
   console.log(socket.remoteAddress, '접속.');
   socket.addListener('data', function(data){
     console.log(data.toString());
+    // socket.write('서버로부터...' + data);
+    socket.write('HTTP/1.1 200 OK\n');
+    socket.write('Content-Type: text/html;charset=utf-8\n');
+    socket.write('\n');
+    socket.end('<h1>안녕 HTTP 서버</h1>');
   });
 }).listen(8000, function(){
   console.log('TCP 서버 구동 완료.');
