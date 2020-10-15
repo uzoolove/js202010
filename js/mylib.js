e007ebb664cbf01fb38a95701676c77a335ed204
uzoolove <uzoolove@gmail.com>
2020-10-15 11:33:29 +0900

    .

---
+++ js/mylib.js
@@ -0,0 +1,21 @@
+// Ajax 요청을 보낸다.
+var ajax = function(url, method, async, data, dataType, success){
+  if(method == 'get' && data){
+    url += '?' + data;
+    data = '';
+  }
+  // 1. XMLHttpRequest 생성		
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function(){
+    // 4. 응답 데이터 처리
+    var result = xhr.responseText;
+    if(dataType == 'json'){
+      result = JSON.parse(result);
+    }    
+    success(result);
+  };
+  // 2. 요청준비(open())
+  xhr.open(method, url, async);
+  // 3. 요청(send())
+  xhr.send(data);
+};
