2ccd62561005e71329d5d8589e91a8a7474f49b1
uzoolove <uzoolove@gmail.com>
2020-10-15 15:10:10 +0900

    .

--- etc/primetest.js
+++ etc/primetest.js
@@ -1,6 +1,22 @@
 // 지정한 수가 소수인지 여부를 반환
 var isPrime = function(num){
-	
+  // 캐시를 위한 코드
+  isPrime.cache = isPrime.cache || {};
+  if(isPrime.cache[num] != undefined){
+    return isPrime.cache[num];
+  }else{
+    // 소수 판별 코드
+    var prime = true;
+    for(var i=2; i<=num/2; i++){
+      if(num%i == 0){
+        prime = false;
+        break;
+      }
+    }
+  }  
+  // 소수 판별 코드
+  isPrime.cache[num] = prime;
+  return prime;
 };
 
 console.time('소요시간');
