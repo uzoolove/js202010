85082c9d4632485cd1d53b3c165ec4576454c501
uzoolove <uzoolove@gmail.com>
2020-10-16 14:49:43 +0900

    .

--- ch04/ex04-26-03.js
+++ ch04/ex04-26-03.js
@@ -1,1 +1,23 @@
 // ex04-26.js 복사
+var count = 0;
+var myObj = {
+	count: 0,
+	visit: function(){
+		// 방문자를 한명 증가시킨다.
+    this.count++; // this=myObj
+    var visitN = function(n){
+      this.count += n; // this=window
+    };
+    visitN.call(this, 2);
+    visitN.call(this, 2);
+
+    var visit3 = visitN.bind(this, 3);
+    visit3();
+    visit3();
+	}
+};
+
+myObj.visit();
+myObj.visit();
+console.log('합계', myObj.count);
+
--- etc/overridetest.js
+++ etc/overridetest.js
@@ -1,1 +1,40 @@
+// 메모이제이션(캐싱) 기능의 함수를 정의할때 사용
+// isPrime(5); 캐싱 X
+// isPrime.memoization(5);  캐싱 O
+Function.prototype.memoization = function(key){
+  // 캐시를 위한 코드
+  this.cache = this.cache || {};
+  if(this.cache[key] != undefined){
+    return this.cache[key];
+  }else{
+    // 캐시를 위한 코드
+    return this.cache[key] = this(key);
+  }
+};
+
 // primetest.js 복사
+// 지정한 수가 소수인지 여부를 반환
+var isPrime = function(num){
+  // 소수 판별 코드
+  var prime = true;
+  for(var i=2; i<=num/2; i++){
+    if(num%i == 0){
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+};
+
+console.time('소요시간');
+console.log('3 -> ', isPrime(3));
+console.log('4 -> ', isPrime(4));
+console.log('5 -> ', isPrime(5));
+console.log('6 -> ', isPrime(6));
+console.log('7 -> ', isPrime(7));
+console.log('8 -> ', isPrime(8));
+console.log('9 -> ', isPrime(9));
+console.log('1000000007 -> ', isPrime.memoization(1000000007));
+console.log('1000000007 -> ', isPrime.memoization(1000000007));
+console.log('1000000007 -> ', isPrime.memoization(1000000007));
+console.timeEnd('소요시간');
